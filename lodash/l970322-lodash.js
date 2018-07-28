9c03c8d7c874f9ba4dbe7ebd9199cf5d070488dc
li <li@cddm.me>
2018-07-28 19:52:48 +0800

    js

--- lodash/l970322-lodash.js
+++ lodash/l970322-lodash.js
@@ -82,10 +82,8 @@ var l970322 = {
     for (var i = 0 ; i < array.length ; i++){
       if(!Array.isArray(array[i])){
         result.push(array[i])
-      }
-    }else{
-      for(var j = 0 ; j < array[i].length ; j++){
-        result.push(array[i][j])
+      }else(){
+        result.push(array[i])
       }
     }
     return result
